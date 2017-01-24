311e1b06226ec6902fa33e310bd7179a8e9e94a9
Justin Kenel <jfkenel@gmail.com>
2017-01-24 14:01:00 -0500

    BugFix: Add missing syntax
    Add in missing semicolons (not explicit bugs, but good to have).
    
    Add missing parenthesis and braces so that server.js can start.

--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -5,22 +5,23 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/favorites', function(req, res){
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-;
+});
 
 app.get('favorites', function(req, res){
   if(!req.body.name || !req.body.oid){
     res.send("Error");
-    return
-  
+    return;
+  }
+
   var data = JSON.parse(fs.readFileSync('./data.json'));
   data.push(req.body);
   fs.writeFile('./data.json', JSON.stringify(data));
