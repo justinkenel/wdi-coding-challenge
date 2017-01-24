7aa900cf5e6a3c248d0403626ce0ec6edb8881b2
Justin Kenel <jfkenel@gmail.com>
2017-01-24 15:12:38 -0500

    Update submit behavior to populate list of movies
    Created searchOmdb - a callback based function for getting search
    results from the omdb API - it has an onLoad parameter for handling
    the results.
    
    The eventListener for the form uses the value of the search text box
    to construct the url for hitting the search API. Once the results
    are returned it calls the success callback with results.
    
    The success callback currently creates an `li` element and populates
    them into the movie list `ul` element.

--- node-backend-starter-code/public/movie-list.js
+++ node-backend-starter-code/public/movie-list.js
@@ -1,5 +1,48 @@
-function buildMovieList(searchBox, movieList) {
-  searchBox.addEventListener("submit", function(event) {
-    console.log("This is another test");
+function buildMovieList(searchForm, movieList) {
+  function searchOmdb(searchText, onLoad, onError) {
+    var searchUrl = 'http://www.omdbapi.com/?type=movie&s=';
+    var searchRequest = new XMLHttpRequest();
+    var requestUrl = searchUrl + encodeURIComponent(searchText.trim());
+    searchRequest.open("GET", requestUrl, true);
+
+    searchRequest.onload = function(event) {
+      var results = JSON.parse(searchRequest.responseText);
+      onLoad(results);
+    };
+    searchRequest.onerror = function(event) {
+      onError(searchRequest);
+    }
+    searchRequest.send(null);
+  }
+
+  function buildMovieElement(movieData) {
+    var title = movieData.Title;
+    var year = movieData.Year;
+    var id = movieData.imdbID;
+
+    var movieElement = document.createElement('li');
+    movieElement.innerHTML = title;
+
+    return movieElement;
+  }
+
+  searchForm.addEventListener("submit", function(event) {
+    var searchText = searchForm.elements['searchText'].value;
+    searchOmdb(searchText, function(results) {
+      console.log("OnLoad: " + JSON.stringify(results[0]));
+
+      // remove all elements currently in the search results
+      movieList.innerHTML = "";
+
+      // iterate over all results from the search:
+      //  - build an li element to go in the movie list element
+      //  - append the constructed element to the list
+      var searchValues = results['Search'];
+      for(var i in searchValues) {
+        var li = buildMovieElement(searchValues[i]);
+        movieList.append(li);
+      }
+    });
+    return false;
   });
 }
